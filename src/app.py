2e5544e7ae26597241b1f5d1770829b9c4e1bf1d
Ayush katiyar <katiyar.ash21@gmail.com>
2026-02-06 15:39:21 +0530

    Modified the project for Streamlit Cloud deployment.

--- src/app.py
+++ src/app.py
@@ -19,13 +19,63 @@ class HealthDataInput(BaseModel):
     screen_time: float
 
 # creating a dummy model
+# def create_dummy_model():
+#   from sklearn.ensemble import RandomForestClassifier
+#   model = RandomForestClassifier(n_estimators=100,random_state=42)
+#   X = np.random.rand(100,6)
+#   Y = np.random.choice(['Low risk','medium risk','High risk'],size = 100)
+#   model.fit(X,Y)
+#   return model
 def create_dummy_model():
-  from sklearn.ensemble import RandomForestClassifier
-  model = RandomForestClassifier(n_estimators=100,random_state=42)
-  X = np.random.rand(100,6)
-  Y = np.random.choice(['Low risk','medium risk','High risk'],size = 100)
-  model.fit(X,Y)
-  return model
+    from sklearn.ensemble import RandomForestClassifier
+    import numpy as np
+
+    np.random.seed(42)
+    n_samples = 200
+
+    sleep_hours = np.random.uniform(4, 9, size=n_samples)
+    stress_level = np.random.randint(1, 10, size=n_samples)
+    social_activity = np.random.randint(0, 7, size=n_samples)
+    work_hours = np.random.uniform(4, 12, size=n_samples)
+    screen_time = np.random.uniform(2, 10, size=n_samples)
+    physical_activity = np.random.uniform(0, 7, size=n_samples)
+
+    X = np.column_stack([
+        sleep_hours,
+        stress_level,
+        social_activity,
+        work_hours,
+        screen_time,
+        physical_activity
+    ])
+
+    # Create scores
+    score_values = (
+        (9 - sleep_hours) +         
+        stress_level * 1.5 +        
+        screen_time +              
+        (12 - work_hours) * 0.5 -  
+        physical_activity -        
+        social_activity
+    )
+
+    # Divide by percentiles
+    low_th = np.percentile(score_values, 33)
+    high_th = np.percentile(score_values, 66)
+
+    Y = []
+    for score in score_values:
+        if score < low_th:
+            Y.append('Low risk')
+        elif score < high_th:
+            Y.append('medium risk')
+        else:
+            Y.append('High risk')
+
+    model = RandomForestClassifier(n_estimators=100, random_state=69)
+    model.fit(X, Y)
+
+    return model
 
 #initialize the dummy_model
 model = create_dummy_model()
